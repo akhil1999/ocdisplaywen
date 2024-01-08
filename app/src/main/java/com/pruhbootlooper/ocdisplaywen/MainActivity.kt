640f9f4ef2a824eeba594202db9f4cc4fc4a68f8
akhil1999 <akhileshbhuvanendra@gmail.com>
2024-01-08 12:32:53 +0530

    Add profile dropdown

--- app/src/main/java/com/pruhbootlooper/ocdisplaywen/MainActivity.kt
+++ app/src/main/java/com/pruhbootlooper/ocdisplaywen/MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.SeekBar
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -104,6 +107,16 @@ class MainActivity : AppCompatActivity() {
             S += 1
             Stext.text = S.toString()
             Sseek.progress = S
+        }
+
+        val spProfiles : Spinner = findViewById(R.id.spProfiles)
+        spProfiles.onItemSelectedListener = object  : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                showToast(this@MainActivity, "Loaded ${adapterView?.getItemAtPosition(position).toString()}", Toast.LENGTH_SHORT)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
         }
 
         //Check if root exist and only then proceed else throw blocking dialog box.
